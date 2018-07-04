13a2ba911b49c6e45af639efdd362cf52ec19f22
Denmark Garcia <garciadenmark.x@gmail.com>
2018-07-04 11:38:46 +0800

    Added address_book app

--- src/urls.py
+++ src/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	path('', views.index,name='index'),
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
+    path('address_book/', include('address_book.urls')),
 ]
