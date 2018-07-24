de16e971bc0fc206390008fc77121a7bb5b6ae02
Denmark Garcia <garciadenmark.x@gmail.com>
2018-07-24 12:39:59 +0800

    Add to admin.py a class

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from accounts.models import AddressBook
+from accounts.models import  News, AddressBook
+
+class AddressBooksAdmin(admin.ModelAdmin):
+	class Meta:
+		fields = AddressBook
+
+	search_fields = ['first_name','phone',]
+
+	list_display = (
+		'first_name', 'last_name', 'phone', 'address'
+
+	)
+
+class NewsAdmin(admin.ModelAdmin):
+	class Meta:
+		fields = News
+		verbose_name_plural = "News"
+
+	list_display = [
+		'headline', 'author', 'pub_date','body',
+	]
 
 
-
-admin.site.register(AddressBook)
+admin.site.register(AddressBook, AddressBooksAdmin)
+admin.site.register(News, NewsAdmin)
+admin.site.site_header = ("School System Login")
