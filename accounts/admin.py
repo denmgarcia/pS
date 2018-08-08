6be19168adf700eb81902d43bc4238d02e98e363
Denmark Garcia <dengarcia.x@gmail.com>
2018-08-08 09:23:21 +0800

    Comply withPEP8

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,30 +1,31 @@
 from django.contrib import admin
-from accounts.models import  News, AddressBook
+from accounts.models import News, AddressBook
+
 
 class AddressBooksAdmin(admin.ModelAdmin):
-	class Meta:
-		fields = AddressBook
+    class Meta:
+        fields = AddressBook
 
-	search_fields = ['first_name','phone',]
+    search_fields = ['first_name', 'phone', ]
 
-	list_display = (
-		'first_name', 'last_name', 'phone', 'address'
+    list_display = (
+        'first_name', 'last_name', 'phone', 'address'
 
-	)
+    )
 
-	list_filter = (list_display)
+    list_filter = (list_display)
 
 
 class NewsAdmin(admin.ModelAdmin):
-	class Meta:
-		fields = News
-		verbose_name_plural = "News"
+    class Meta:
+        fields = News
+        verbose_name_plural = "News"
 
-	list_display = [
-		'headline', 'author', 'pub_date','body',
-	]
-	list_filter = ('headline', 'author', 'pub_date',)
-	ordering = ('-pub_date',)
+    list_display = [
+        'headline', 'author', 'pub_date', 'body',
+    ]
+    list_filter = ('headline', 'author', 'pub_date',)
+    ordering = ('-pub_date',)
 
 
 admin.site.register(AddressBook, AddressBooksAdmin)
