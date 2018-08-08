84f23aa42804cddb2c192fa3074faaff6359fb8c
Denmark Garcia <dengarcia.x@gmail.com>
2018-08-08 09:15:07 +0800

    Comply to PEP8

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,28 +1,28 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.forms import ModelForm
 from accounts.models import AddressBook
 
 
 class StudentForm(forms.ModelForm):
     class Meta:
         model = AddressBook
-        fields = ('first_name','last_name','phone','address')
-        
+        fields = ('first_name', 'last_name', 'phone', 'address')
+
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "Username", "required":"autofocus"})) 
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control", "placeholder": "Password","required":"autofocus"})) 
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username", "required": "autofocus"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password", "required": "autofocus"}))
 
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True,widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "Email", "required":"autofocus"}) )
-    username = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "Username", "required":"autofocus"}))
-    first_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "First Name", "required":"autofocus"}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "Last Name", "required":"autofocus"}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control", "placeholder": "Password","required":"autofocus"}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control", "placeholder": "Confirm Password","required":"autofocus"}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Email", "required": "autofocus"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username", "required": "autofocus"}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "First Name", "required": "autofocus"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Last Name", "required": "autofocus"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password", "required": "autofocus"}))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Confirm Password", "required": "autofocus"}))
 
     class Meta:
         model = User
@@ -35,8 +35,6 @@ class RegistrationForm(UserCreationForm):
             'password2')
 
 
-
-
 def save(self, commit=True):
     user = super(RegistrationForm, self).save(commit=False)
     user.first_name = self.clean_data['first_name']
@@ -45,4 +43,4 @@ def save(self, commit=True):
 
     if commit:
         user.save()
-    return user
+        return user
--- accounts/models.py
+++ accounts/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
-class AddressBook(models.Model):
-	first_name = models.CharField(max_length=50, default='')
-	last_name = models.CharField(max_length=50,default='')
-	phone = models.CharField(max_length=11, default='')
-	address = models.TextField(max_length=100)
 
-	def __str__(self):
-		return '{} {}'.format(self.first_name,self.last_name)
+class AddressBook(models.Model):
+    first_name = models.CharField(max_length=50, default='')
+    last_name = models.CharField(max_length=50, default='')
+    phone = models.CharField(max_length=11, default='')
+    address = models.TextField(max_length=100)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
 
 class News(models.Model):
-	headline = models.CharField(max_length=40, default='')
-	author = models.CharField(max_length=50, default='')
-	pub_date = models.DateTimeField('date published')
-	body = models.TextField()
+    headline = models.CharField(max_length=40, default='')
+    author = models.CharField(max_length=50, default='')
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
 
-	def __str__(self):
-		return self.headline
-
+    def __str__(self):
+        return self.headline
