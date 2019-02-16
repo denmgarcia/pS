a8b7345cec9ad7d3a2c392669185c240e30b5ae1
Denmark Garcia <denmark@philsmile.com>
2019-02-16 09:29:58 +0800

    Added djdt

--- src/settings.py
+++ src/settings.py
@@ -14,6 +14,7 @@ SECRET_KEY = '1_fs=!w2_3340swpxk2v&=$7!99ww8wb7=$x@)=j159%y%-482'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+INTERNAL_IPS = ['127.0.0.1']
 
 
 # Application definition
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'accounts',
     'django_extensions',
 
@@ -39,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'src.urls'
--- src/urls.py
+++ src/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 from accounts import views
 from accounts.views import IndexView
+from django.conf import settings
 
 
 
@@ -10,3 +11,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
 ]
+
+if settings.DEBUG:
+	import debug_toolbar
+	urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
